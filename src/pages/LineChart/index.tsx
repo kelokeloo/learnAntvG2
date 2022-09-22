b2dac9cd65bca5015cee2a62036fa5dc2ab38e27
lihongye <lihongye@cvte.com>
2022-09-22 20:54:01 +0800

    添加AxisPointer指示器

--- src/pages/LineChart/index.tsx
+++ src/pages/LineChart/index.tsx
@@ -1,9 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
 import aapl from "./Data/aapl.json";
 import * as d3 from "d3";
-import { useCreation, useSize } from "ahooks";
+import { useCreation, useSetState, useSize } from "ahooks";
+import moment from "moment";
+import { round } from "lodash";
 
 type TAapl = { date: string; close: number };
+
+type TDimensions =
+  | {
+      width: number;
+      height: number;
+      margin: {
+        top: number;
+        right: number;
+        bottom: number;
+        left: number;
+      };
+    }
+  | undefined;
 
 type AxisProps = {
   position: { x: number; y: number } | undefined;
@@ -41,23 +56,13 @@ const YAxis = (props: YAxisProps) => {
 };
 
 type LineProps = {
-  xScale: d3.ScaleTime<number, number, never> | undefined;
-  yScale: d3.ScaleLinear<number, number, never> | undefined;
   data: TAapl[];
+  serializer: d3.Line<TAapl> | undefined;
 };
 
 const Line = (props: LineProps) => {
-  const { xScale, yScale, data } = props;
+  const { data, serializer } = props;
   const [lineNode, setLineNode] = useState<SVGPathElement | null>(null);
-
-  const serializer = useCreation(() => {
-    if (xScale && yScale) {
-      return d3
-        .line<TAapl>()
-        .x((d) => xScale(new Date(d.date)))
-        .y((d) => yScale(d.close));
-    }
-  }, [xScale, yScale]);
 
   const refCallback: React.LegacyRef<SVGPathElement> | undefined = (el) => {
     setLineNode(el);
@@ -75,15 +80,123 @@ const Line = (props: LineProps) => {
   return <path ref={refCallback}></path>;
 };
 
-const LineChart = () => {
-  useEffect(() => {
-    console.log("aapl", aapl);
-  }, []);
+type TLine = {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+};
+// 绘制AxisPointer
+type AxisPointerProps = {
+  innerWidth: number | undefined;
+  innerHeight: number | undefined;
+  cursorPos:
+    | {
+        offsetX: number;
+        offsetY: number;
+      }
+    | undefined;
+  xScale: d3.ScaleTime<number, number, never> | undefined;
+  yScale: d3.ScaleLinear<number, number, never> | undefined;
+  serializer: d3.Line<TAapl> | undefined;
+  dimensions: TDimensions;
+};
 
+const AxisPointer = (props: AxisPointerProps) => {
+  const {
+    innerWidth,
+    innerHeight,
+    cursorPos,
+    xScale,
+    yScale,
+    serializer,
+    dimensions,
+  } = props;
+  //
+  const pointerInfo = useCreation(() => {
+    if (
+      innerWidth &&
+      innerHeight &&
+      cursorPos &&
+      xScale &&
+      yScale &&
+      serializer &&
+      dimensions
+    ) {
+      const curDate = xScale.invert(cursorPos.offsetX);
+      // 二分访问器
+      const bisect = d3.bisector<TAapl, Date>((d) => new Date(d.date)).left;
+      const yOffset = serializer.y()(aapl[bisect(aapl, curDate)], 0, aapl);
+
+      //
+      const curMoment = moment(curDate);
+      const curClose = yScale.invert(yOffset);
+      const xLabel = curMoment.format("YYYY-MM-DD");
+      const yLabel = round(curClose);
+
+      const horizontal: TLine = {
+        x1: 0,
+        y1: yOffset,
+        x2: innerWidth,
+        y2: yOffset,
+      };
+      const vertical: TLine = {
+        x1: cursorPos.offsetX,
+        y1: 0,
+        x2: cursorPos.offsetX,
+        y2: innerHeight,
+      };
+
+      const horizontalLabel = {
+        x: 0 - dimensions.margin.left,
+        y: yOffset,
+        label: yLabel,
+      };
+
+      const verticalLabel = {
+        x: cursorPos.offsetX,
+        y: innerHeight + 16,
+        label: xLabel,
+      };
+
+      return {
+        lines: [horizontal, vertical],
+        labels: [horizontalLabel, verticalLabel],
+      };
+    }
+  }, [
+    innerWidth,
+    innerHeight,
+    cursorPos,
+    xScale,
+    yScale,
+    serializer,
+    dimensions,
+  ]);
+  return (
+    <g>
+      {pointerInfo?.lines?.map((item, index) => (
+        <line key={index} {...item} stroke="#e0e0e0"></line>
+      ))}
+      {pointerInfo?.labels?.map((item, index) => (
+        <text
+          x={item.x}
+          y={item.y}
+          fill="black"
+          style={{ backgroundColor: "black" }}
+        >
+          {item.label}
+        </text>
+      ))}
+    </g>
+  );
+};
+
+const LineChart = () => {
   const ref = useRef(null);
   const size = useSize(ref);
 
-  const dimensions = useCreation(() => {
+  const dimensions: TDimensions = useCreation(() => {
     if (size) {
       const { width } = size;
       return {
@@ -163,18 +276,83 @@ const LineChart = () => {
   }, [xDomain, xRange]);
 
   // 画线
+  const serializer = useCreation(() => {
+    if (xScale && yScale) {
+      return d3
+        .line<TAapl>()
+        .x((d) => xScale(new Date(d.date)))
+        .y((d) => yScale(d.close));
+    }
+  }, [xScale, yScale]);
+
+  const [svgContainer, setSvgContainer] = useState<SVGGElement | null>(null);
+  const svgCallback: React.LegacyRef<SVGGElement> | undefined = (el) => {
+    setSvgContainer(el);
+  };
+
+  // 绘制AxisPointer
+  // 捕获svg上的事件
+  const [svgOffset, setSvgOffset] = useSetState({
+    offsetX: 0,
+    offsetY: 0,
+  });
+
+  useEffect(() => {
+    if (svgContainer) {
+      d3.select(svgContainer).on("mousemove", (event: MouseEvent, d) => {
+        if (event) {
+          const { offsetX, offsetY } = event;
+          setSvgOffset({
+            offsetX,
+            offsetY,
+          });
+        }
+      });
+    }
+  }, [svgContainer]);
+
+  const cursorPos = useCreation(() => {
+    if (svgOffset && dimensions && innerWidth && innerHeight) {
+      let offsetX: number = svgOffset.offsetX - dimensions.margin.left;
+      let offsetY: number = svgOffset.offsetY - dimensions.margin.top;
+      offsetX = offsetX < 0 ? 0 : offsetX;
+      offsetX = offsetX > innerWidth ? innerWidth : offsetX;
+      // offsetY
+      offsetY = offsetY < 0 ? 0 : offsetY;
+      offsetY = offsetY > innerHeight ? innerHeight : offsetY;
+      return {
+        offsetX,
+        offsetY,
+      };
+    }
+  }, [svgOffset, dimensions, innerWidth, innerHeight]);
 
   return (
-    <div ref={ref}>
-      <svg width={dimensions?.width} height={dimensions?.height}>
+    <div ref={ref} style={{ position: "relative" }}>
+      <svg
+        width={dimensions?.width}
+        height={dimensions?.height}
+        ref={svgCallback}
+      >
         <g
           width={innerWidth}
           height={innerHeight}
-          transform={`translate(${mainContainerOffset?.x} ${mainContainerOffset?.y})`}
+          transform={`translate(${mainContainerOffset?.x ?? 0} ${
+            mainContainerOffset?.y ?? 0
+          })`}
         >
           <XAxis scale={xScale} position={xAxisOffset} />
           <YAxis scale={yScale} />
-          <Line data={aapl} xScale={xScale} yScale={yScale} />
+          <Line data={aapl} serializer={serializer} />
+          <AxisPointer
+            innerWidth={innerWidth}
+            innerHeight={innerHeight}
+            cursorPos={cursorPos}
+            xScale={xScale}
+            yScale={yScale}
+            serializer={serializer}
+            dimensions={dimensions}
+          />
         </g>
       </svg>
     </div>
